289bdcc876c88061b36368b861308edafbf47ec4
Johannes Carlsen <hannes.mcman@gmail.com>
2018-11-15 13:08:10 -0600

    lock body scroll (fixes mobile issue)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import {disableBodyScroll} from 'body-scroll-lock'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+const targetElement = document.getElementById('root')
+disableBodyScroll(targetElement)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
