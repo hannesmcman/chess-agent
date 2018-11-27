f5fe455d40d42ed3b787c00e6c538e6f7aafc041
Johannes Carlsen <hannes.mcman@gmail.com>
2018-11-27 15:18:40 -0600

    Updates

---
+++ precache-manifest.da865d70a745dc4a66d404c509120ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "094ed3ddbf7d284fed12",
+    "url": "/chess-agent/static/js/runtime~main.094ed3dd.js"
+  },
+  {
+    "revision": "9a387e0d52a137591802",
+    "url": "/chess-agent/static/js/main.9a387e0d.chunk.js"
+  },
+  {
+    "revision": "b340363a8fab5cfd6e79",
+    "url": "/chess-agent/static/js/1.b340363a.chunk.js"
+  },
+  {
+    "revision": "9a387e0d52a137591802",
+    "url": "/chess-agent/static/css/main.f1225030.chunk.css"
+  },
+  {
+    "revision": "b340363a8fab5cfd6e79",
+    "url": "/chess-agent/static/css/1.22825121.chunk.css"
+  },
+  {
+    "revision": "579a4ed1a551b785268f569931bc0ce7",
+    "url": "/chess-agent/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chess-agent/precache-manifest.be944260bb709856f8c3fe333c671ddb.js"
+  "/chess-agent/precache-manifest.da865d70a745dc4a66d404c509120ab1.js"
 );
 
 workbox.clientsClaim();
