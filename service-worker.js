d6ff9accc34600bee6f9e51031775f402dbd82a5
Johannes Carlsen <hannes.mcman@gmail.com>
2018-11-27 16:16:12 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chess-agent/precache-manifest.64a179ca800112ae1a5b078a34931121.js"
+  "/chess-agent/precache-manifest.758851a436f9634e8640cf7bdbec034b.js"
 );
 
 workbox.clientsClaim();
